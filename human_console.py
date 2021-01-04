41ed2721c533268529cf0622aa67ba59d3b4f328
Krystian Kabat <krystian.kabat97@gmail.com>
2021-01-04 21:02:09 +0100

    dodano tworzenie pliku tekstowego z podaną nazwą

--- human_console.py
+++ human_console.py
@@ -1,25 +1,60 @@
 from ply.lex import lex
 from ply.yacc import yacc
 import re
+import os
 
 tokens = (
     'OPERATE',
+    'OBJECT',
+    'FILE_TYPE',
+    'FILE_NAME'
 )
 
 
 def t_OPERATE(t):
-    r'[Ww](łącz|yłącz) | [Oo](dpal|dtwórz|dpaułzuj) | [Zz](atrzymaj|apałzuj)'
+    r'[Ww](łącz|yłącz) | [Oo](dpal|dtwórz|dpaułzuj) | [Zz](atrzymaj|apałzuj) | [SsUu]twórz | [Ss]zukaj'
     return t
+
+
+def t_OBJECT(t):
+    r'[Pp]lik'
+    return t
+
+
+def t_FILE_TYPE(t):
+    r'[Tt]ekstowy | [Dd]źwiękowy'
+    return t
+
+
+def t_FILE_NAME(t):
+    r'\w+'
+    if re.match(r'\w+', t.value):
+        return t
+    else:
+        t.value = 'file'
+        return t
 
 
 t_ignore = ' \t'
 
 
 def p_command(p):
-    '''command : OPERATE'''
-    print(re.match(r'[Ww]łącz|[Oo](dpal|dtwórz)', p[1]), p[1])
+    '''command : OPERATE
+               | OPERATE OBJECT FILE_TYPE FILE_NAME
+               | OPERATE OBJECT FILE_TYPE'''
+
     if re.match(r'[Ww]łącz|[Oo](dpal|dtwórz)', p[1]):
         print("Elo jestem se tutaj. Testuje se.")
+
+    elif re.match(r'[SsUu]twórz', p[1]):
+        if re.match(r'[Pp]lik', p[2]):
+            if re.match(r'[Tt]ekstowy', p[3]):
+                f = open(p[4] + ".txt", 'w')
+                f.close()
+                print("Stworzono plik :D !")
+        else:
+            print("Zła komenda :(")
+
 
 
 lexer = lex()
